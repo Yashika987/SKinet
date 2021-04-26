c60a511a22064db900bff91debe2a60a3d635913
Yashika Maheshwari <yyashika.m1996@outlook.com>
2021-04-26 20:10:36 +0530

    Images

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -13,7 +13,10 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
-using API.Data;
+using Core.Interfaces;
+using Core.Specification;
+using API.Dtos;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -21,26 +24,54 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
+        IGenericRepository<Products> _productsRepo;
+        IGenericRepository<ProductBrand> _productBrandRepo;
+        IGenericRepository<ProductType> _productTypeRepo;
+        IMapper _mapper;
+        public ProductController(IGenericRepository<Products> productsRepo, 
+        IGenericRepository<ProductBrand> productBrandRepo, 
+        IGenericRepository<ProductType> productTypeRepo, IMapper mapper)
+        {
+            _mapper=mapper;
+            _productsRepo=productsRepo;
+            _productBrandRepo=productBrandRepo;
+            _productTypeRepo=productTypeRepo;
 
-      private readonly StoreContext _context;
-      public ProductController(StoreContext context)
-      {
-           _context=context;
-      }
+        }
+
+    //   private readonly StoreContext _context;
+    //   public ProductController(StoreContext context)
+    //   {
+    //        _context=context;
+    //   }
       [HttpGet]
-      public async Task <ActionResult<List<Products>>> GetProducts()
+      public async Task <ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
       {
-           var products = await _context.Products.ToListAsync();
-          return Ok(products);
+          var spec =new ProductsWithTypesAndBrandsSpecification();
+           var products = await _productsRepo.ListAsync(spec);
+          return Ok(_mapper.Map<IReadOnlyList<Products>, IReadOnlyList<ProductToReturnDto>>
+          (products));
       }
       [HttpGet("{id}")]
-      public async Task <ActionResult<Products>> GetProducts(int id)
+      public async Task <ActionResult<ProductToReturnDto>> GetProducts(int id)
       {
-           
+          var spec =new ProductsWithTypesAndBrandsSpecification(id);
+          var product= await _productsRepo.GetEntityWithSpec(spec);
+          return _mapper.Map<Products ,ProductToReturnDto >(product);
 
-          return await _context.Products.FindAsync(id);
       }
-
+      
+       [HttpGet("brands")]
+       public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+       {
+           
+           return Ok(await _productBrandRepo.ListAllAsync());
+       }
+     [HttpGet("types")]       
+     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
+       {
+           return Ok(await _productTypeRepo.ListAllAsync());
+       }
        
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
+using API.Helpers;
+using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,11 +29,15 @@ namespace API
         
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddScoped<IProductRepository, ProdctRepository>();
+            services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext>(x => 
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,6 +57,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
